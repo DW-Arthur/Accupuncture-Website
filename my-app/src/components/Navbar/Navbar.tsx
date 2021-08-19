48cf03eb7a88fde752c9847a080c56c760315f46
unknown <Wangshujian1999@gmail.com>
2021-08-18 23:08:06 -0400

    added styleDiv NavBar

--- my-app/src/components/Navbar/Navbar.tsx
+++ my-app/src/components/Navbar/Navbar.tsx
@@ -2,26 +2,64 @@ import Reac, { Component } from "react";
 import './Navbar.css'
 import {MenuItems} from "./MenuItems"
 import { isTemplateElement } from "@babel/types";
+import styled from 'styled-components';
+import { ButtonGroup, Button } from '@material-ui/core';
+
+const Yolo = styled.button`
+  display: inline-block;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  display: block;
+`;
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 5rem;
+`
+
+const StyledButton = styled(Button)`
+  height: 3rem;
+  width: 15rem;
+  color: blue;
+  
+
+`
+
 
 class Navbar extends Component{ //class used for states instead of functions
     render(){ //This is needed for Class?
         return(
-            <nav className="NavbarItems">
+            <div>
+                <nav className="NavbarItems">
                 <h1 className ="navbar-logo">React</h1>
                 <div className="menu-icon">
                     
                 </div>
                 <ul>
-                <li><a href="#home" >Home</a></li>
-                    <li><a href="#news">Appointment</a></li> 
-                    <li><a href="#contact">Contact</a></li>
-                    <li><a href="#about">About</a></li>
+                
+                    <li><Yolo as ="a" href="#book">Book Now</Yolo></li>
                 </ul>
                 
 
 
 
             </nav>
+    
+            <StyledDiv>
+                <ButtonGroup style={{color:'red'}}>
+                    <StyledButton variant='contained' href='/'>Home</StyledButton>
+                    <StyledButton variant='contained' href='/about'>About Me</StyledButton>
+                    <StyledButton variant='contained' href='/experience'>Experience</StyledButton>
+                    <StyledButton variant='contained' href='/contact'>Contact Me</StyledButton>
+                </ButtonGroup>
+            </StyledDiv>
+            </div>
+            
 
         )
 
